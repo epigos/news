eecce483693290a63b4238e8d08d337f8ec219c9
Philip Adzanoukpe <epigos@gmail.com>
2015-12-05 19:43:28 +0000

    add base spider

---
+++ news/utils/linkextractors/__init__.py
@@ -0,0 +1,16 @@
+import pyquery
+
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+
+
+class LinkExtractor(SgmlLinkExtractor):
+
+    def __init__(self, selector=None, type='css', *args, **kwargs):
+        if selector:
+            if type not in ['css', 'xpath']:
+                raise Exception('Selector type not supported.')
+            if type == 'xpath':
+                kwargs['restrict_xpaths'] = selector
+            else:
+                kwargs['restrict_xpaths'] = pyquery.PyQuery('a')._css_to_xpath(selector)
+        SgmlLinkExtractor.__init__(self, *args, **kwargs)
